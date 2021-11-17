de6ac59cde5cbc5b94282ac0a8b7dea0a01a79be
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-11-17 21:07:55 +0700

    asdsfsdf

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
 
 function Home() {
-  const [car, setCar] = useState(0);
+  const [car, setCar] = useState('');
   const [loading, setLoading] = useState(false);
   const [number, setNumber] = useState('eeb72z');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setCar('');
     check();
   };
   const check = async () => {
@@ -42,7 +43,7 @@ function Home() {
           <button type='submit'>Submit</button>
         </form>
         <div>{loading && 'loading...'}</div>
-        <div>{!loading && car}</div>
+        <div>{!loading && <div>car: {car}</div>}</div>
       </div>
     </>
   );
